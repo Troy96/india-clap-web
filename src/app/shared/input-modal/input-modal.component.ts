1c1a3c735d6450f780d850c3a8f1f4b940da58fa
Tuhin Roy <troy0870@gmail.com>
2020-04-16 21:07:58 +0530

    add dynamic form for myProfile

--- src/app/shared/input-modal/input-modal.component.ts
+++ src/app/shared/input-modal/input-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MyprofileEditableService } from 'src/app/professional-networking/my-profile-editable/myprofile-editable.service';
 import { MyProfile } from 'src/app/professional-networking/my-profile-editable/myprofle';
+import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-input-modal',
@@ -9,16 +10,41 @@ import { MyProfile } from 'src/app/professional-networking/my-profile-editable/m
 })
 export class InputModalComponent implements OnInit {
 
+  inputForm: FormGroup;
+
   inputData: MyProfile
 
   constructor(
-    private myProfileService: MyprofileEditableService
-  ) { }
+    private myProfileService: MyprofileEditableService,
+    private fb: FormBuilder,
+
+  ) {
+    
+  }
 
   ngOnInit() {
+    this.inputForm = this.fb.group({
+      profile: this.fb.array([
+        this.fb.control('')
+      ])
+    });
     this.myProfileService.inputModal$.subscribe(inputData => {
       this.inputData = { ...inputData };
+      this.createDynamicFormControls()
     })
+  }
+
+  createDynamicFormControls() {
+    switch (this.inputData.description) {
+      case 'headline': {
+        // this.inputForm = this.fb.group({
+        //   profile: this.fb.array([
+        //     this.fb.control('')
+        //   ])
+        // });
+        break;
+      }
+    }
   }
 
   closeInputModal() {
