e6e58eba2f1efac6e283b384a4ee68262cdbcbcf
Tuhin Roy <troy0870@gmail.com>
2020-05-18 19:53:58 +0530

    add send message bind

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -8,17 +8,24 @@ import { ChatService } from './chat.service';
 })
 export class ChatComponent implements OnInit {
 
+  messageList: any[];
+
   constructor(
     private _chat: ChatService
   ) { }
 
   ngOnInit() {
-    //this._chat.sendData('test');
     this._chat.getMessages().
       subscribe(data => {
-        console.log(data);
-        this._chat.connect();
+        console.log(data); //Get List of messages. Store it and display it in chat body.
+        this._chat.connect(); //Send the userId of the other user here as argument
       })
+  }
+
+  getUsers() { } //For Side bar users.
+
+  onSendMessage(message: string) {
+    console.log(message);
   }
 
 }
