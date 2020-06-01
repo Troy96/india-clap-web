f541932f5446beab3acd3051fd3065bf79bc5528
Tuhin Roy <troy0870@gmail.com>
2020-06-01 19:17:18 +0530

    add modal fields for invite via email

--- src/app/professional-networking/my-profile-editable/modals/email-invite/email-invite.component.ts
+++ src/app/professional-networking/my-profile-editable/modals/email-invite/email-invite.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthServices } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification.service';
+import { MyprofileEditableService } from '../../myprofile-editable.service';
+import { EducationService } from '../education/education.service';
+import { EmailInviteService } from './email-invite.service';
+
 
 @Component({
   selector: 'app-email-invite',
@@ -7,9 +15,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailInviteComponent implements OnInit {
 
-  constructor() { }
+  emailInviteForm: FormGroup;
 
-  ngOnInit() {
+  ngOnInit() { }
+  isExpire: boolean = true;
+
+  constructor(
+    private _fb: FormBuilder,
+    private _authService: AuthServices,
+    private notifService: NotificationService,
+    private _myProfile: MyprofileEditableService,
+    public _emailInvite: EmailInviteService
+  ) {
+    this.emailInviteForm = this._fb.group({
+      from: [''],
+      to: [''],
+      message: [''],
+    })
   }
 
 }
