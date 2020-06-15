faea5f834687393e7c46092c681c1cd54da4d454
Tuhin Roy <troy0870@gmail.com>
2020-06-15 19:37:53 +0530

    SSL Test: Tried doing 1st approach

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.static('./dist/india-clap-web'));
 
+app.use((req, res, next)=>{
+    if(req.header['x-forwarded-proto'] !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`)
+    }
+    else next();
+});
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/dist/india-clap-web/index.html'));
 });
